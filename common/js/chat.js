4946d6c63eec41e7a76e6150870fd6a24f5c5d63
dwn <danwnielsen@gmail.com>
2021-12-04 23:37:01 -0600

    update

--- common/js/chat.js
+++ common/js/chat.js
@@ -498,7 +498,7 @@ socket.on('chat font', function(msg){
   let langFileURL; $.ajax({async:false,type:'GET',dataType:'text',url:`/lang-file-url/${langFileBasename}`,success:function(r){langFileURL=r;},error:function(r){}});
 
   debug('setAllData 0');
-  setAllData(true, null, null, null);
+  // setAllData(true, null, null, null);
   debug(`Loading font from ${langFileURL}.otf`)
   var newFont = new FontFace(myUser.longId, `url(${langFileURL}.otf)`);
   newFont.load().then(function(loadedFace) {
